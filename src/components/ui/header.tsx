dd253a6bb40e34f34ba6eb998159f53c4b6ae0ad
Vibhav Sharma <40585900+Vib-UX@users.noreply.github.com>
2024-06-26 18:52:04 +0530

    Bug/web3 auth avatar (#6)
    * fix: avatar reloading on connect
    
    * fix: buyNFT biconomy

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useAccount } from 'wagmi';
 
-import useFetchUserDetails from "@/hooks/user/useFetchUserDetails";
+import useFetchUserDetails from '@/hooks/user/useFetchUserDetails';
 import useWeb3AuthWrapper from '@/hooks/useWeb3AuthWrapper';
 
-import AccountConnect from "@/components/layout/header/AccountConnect";
-import Avatar from "@/components/ui/avatar";
+import AccountConnect from '@/components/layout/header/AccountConnect';
+import Avatar from '@/components/ui/avatar';
 
 import useUserOnBoarding from '@/hooks/contracts/useUserOnboarding';
 import { useWeb3Auth } from '@web3auth/modal-react-hooks';
@@ -21,27 +21,27 @@ type props = {
 };
 
 const Header = ({ isOpen, setIsOpen }: props) => {
-  const { userInfo } = useWeb3Auth()
-  useWeb3AuthWrapper()
+  const { userInfo } = useWeb3Auth();
+  useWeb3AuthWrapper();
 
-  const { address } = useAccount()
-  const { data: userData, isLoading, refetch } = useFetchUserDetails()
+  const { address } = useAccount();
+  const { data: userData, isLoading, refetch } = useFetchUserDetails();
 
   const { onBoarding } = useUserOnBoarding({
     onSuccess: () => {
-      refetch()
-    }
-  })
+      refetch();
+    },
+  });
 
   useEffect(() => {
     if (userData) {
       if (!userData?.isFound && userInfo) {
         onBoarding({
-          userInfo
-        })
+          userInfo,
+        });
       }
     }
-  }, [userData?.isFound, userInfo?.name])
+  }, [userData?.isFound, userInfo?.name]);
 
   return (
     <div className='w-full flex items-center border-[1px] border-t-0 border-r-0 h-[110px] border-l-0 border-[#FCC0FF] bg-[#F7F2FA] justify-between px-6 py-4 lg:py-4 fixed top-0 z-10'>
@@ -85,12 +85,13 @@ const Header = ({ isOpen, setIsOpen }: props) => {
         <div className='flex items-end justify-center bg-red-50 text-center'>
           <Image src={logo} priority alt='logo' width={200} height={100} />
           <div className='text-[#000] font-semibold -mb-2 hidden sm:inline-block'>
-            only<span className='text-[#272C8A]'>Fans</span> for <span className="text-[#272C8A]">Web3</span>
+            only<span className='text-[#272C8A]'>Fans</span> for{' '}
+            <span className='text-[#272C8A]'>Web3</span>
           </div>
         </div>
       </Link>
       <div className='flex items-center  justify-end fixed right-0  space-x-2 mx-4'>
-        {address && userInfo && (
+        {userInfo && (
           <Avatar
             userName={userInfo?.name}
             openId={userData?.open_ai_id}
