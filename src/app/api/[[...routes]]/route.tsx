b8c86ae944c0aef70e65285a0b130d5a9fd35a9a
KetanKudikyal <krotusppp@gmail.com>
2024-06-29 21:18:38 +0530

    fix: ui changes

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -37,20 +37,43 @@ app.frame('/', (c) => {
       >
         <div style={{
           display: 'flex',
+          justifyContent: "space-between",
           alignItems: 'center',
           flexDirection: 'row',
         }}>
           <h1>Subscribe to ava adams</h1>
           &nbsp;
           <img src='https://onlytease.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmodel2.a1926d48.webp&w=1920&q=75' alt='ava adams' sizes='20' style={{
-            width: 40,
-            height: 40,
+            width: 80,
+            height: 80,
             borderRadius: '50%', // Make the image round
             overflow: 'hidden', // Add overflow hidden
           }} />
         </div>
         <div style={{ display: 'flex', alignItems: 'center', padding: '0.5rem 0', justifyContent: 'space-between' }}>
           <div style={{ fontSize: '1.25rem', fontWeight: '600' }}>Autopay 🔁</div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem', justifyContent: 'center' }}>
+            <div
+              // onClick={() => {
+              //   setSelectedMonth(s);
+              // }}
+              style={{
+                width: '60px',
+                cursor: 'pointer',
+                borderRadius: '0.5rem',
+                padding: '0.25rem 0.5rem',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                // backgroundColor: selectedMonth === s ? '#0051FE' : '',
+                // color: selectedMonth === s ? 'white' : '',
+                // boxShadow: selectedMonth === s ? '0 4px 6px rgba(0, 0, 0, 0.1)' : '',
+                // border: selectedMonth === s ? '1px solid #0051FE' : ''
+              }}
+            >
+              3m
+            </div>
+          </div>
         </div>
       </div>
     ),
