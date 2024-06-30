ee7e3bb3ce80ccb366160c1e8b3f939e8e83422b
KetanKudikyal <krotusppp@gmail.com>
2024-07-01 03:34:10 +0530

    fix: added frame final

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-key */
 /** @jsxImportSource frog/jsx */
-
+import Big from "big.js"
 import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
 import { neynar } from 'frog/middlewares'
@@ -14,6 +14,11 @@ import { ERC20 } from '@/components/ui/ERC20ABI'
 import { NFT_MARKET_PLACE_ADDRESS } from '@/utils/addresses'
 import { modelCardData } from '@/utils/modelData'
 import { MOCK_USD_BASE } from '@/utils/tokens'
+
+
+type State = {
+  id: string
+}
 
 export const getSubscriptionId = () => {
   const subscriptionId = Math.floor(Math.random() * (1e12 - 1 + 1)) + 1;
@@ -37,10 +42,13 @@ const updateSubscription = async ({ email, subscriptionId, modelId }: {
 }
 
 
-const app = new Frog({
+const app = new Frog<{ State: State }>({
   assetsPath: '/',
   basePath: '/api',
   title: "Only tease",
+  initialState: {
+    id: '2112'
+  }
 })
 
 const neynarMiddleware = neynar({
@@ -51,13 +59,13 @@ const neynarMiddleware = neynar({
 app.frame('/', neynarMiddleware, (c) => {
   const name = c.req.queries('name')?.[0] || '';
   const model = [...modelCardData].find((s) => s.slug.toLowerCase() === name.toLowerCase())
-  const modelId = model?.id.toString()
-  const amount = model?.value.toString()
+
+  const modelId = model?.id?.toString() || '';
+  const amount = model?.value?.toString() || '';
   const subscriptionId = getSubscriptionId()
 
-
   return c.res({
-    action: '/finish',
+    action: `/finish#modelId=${modelId}&subscriptionId=${subscriptionId}`,
     image: (
       <div
         style={{
@@ -199,171 +207,14 @@ app.frame('/', neynarMiddleware, (c) => {
   })
 })
 
-app.frame('/final', async (c) => {
-  const modelId = c.req.queries('modelId')?.[0] || '';
-
-  const model = [...modelCardData].find((s) => s.id.toString() === modelId.toLowerCase())
-
-  const subscriptionId = c.req.queries('subscriptionId')?.[0] || '';
-  const tokenId = parseInt(modelId) * 1e18 + parseInt(subscriptionId)
-  // await updateSubscription({
-  //   email: ,
-  //   modelId: modelId,
-  //   subscriptionId: parseInt(subscriptionId)
-  // })
-  return c.res({
-    image: (
-      <div
-        style={{
-          background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(252, 183, 255, 0.8) 100%)',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          padding: "20px",
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div style={{
-          display: 'flex',
-          justifyContent: "space-between",
-          alignItems: "flex-start",
-          flexDirection: 'row',
-          fontSize: '1.25rem',
-        }}>
-          <h1>You have subscribed to &nbsp; <strong>{model?.name}</strong></h1>
-          &nbsp;
-          <img src={model?.image_url} alt='ava adams' sizes='20' style={{
-            width: 200,
-            height: 200,
-            borderRadius: '50%', // Make the image round
-            overflow: 'hidden', // Add overflow hidden
-          }} />
-        </div>
-        <div style={{ display: 'flex', marginTop: "50px", alignItems: 'center', padding: '0.5rem 0', justifyContent: 'space-between' }}>
-          <div style={{ fontSize: '2.5rem', fontWeight: '600' }}>Autopay 🔁</div>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'center' }}>
-            <div
-              // onClick={() => {
-              //   setSelectedMonth(s);
-              // }}
-              style={{
-                width: '100px',
-                height: "80px",
-                cursor: 'pointer',
-                borderRadius: '0.5rem',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-                display: 'flex',
-                fontSize: '2rem',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#0051FE',
-                color: 'white',
-                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-                border: '1px solid #0051FE'
-              }}
-            >
-              3m
-            </div>
-            <div
-              // onClick={() => {
-              //   setSelectedMonth(s);
-              // }}
-              style={{
-                width: '100px',
-                height: "80px",
-                cursor: 'pointer',
-                borderRadius: '0.5rem',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-                display: 'flex',
-                fontSize: '2rem',
-                alignItems: 'center',
-                justifyContent: 'center',
-                // backgroundColor: selectedMonth === s ? '#0051FE' : '',
-                // color: selectedMonth === s ? 'white' : '',
-                // boxShadow: selectedMonth === s ? '0 4px 6px rgba(0, 0, 0, 0.1)' : '',
-                // border: selectedMonth === s ? '1px solid #0051FE' : ''
-              }}
-            >
-              6m
-            </div>
-            <div
-              // onClick={() => {
-              //   setSelectedMonth(s);
-              // }}
-              style={{
-                width: '100px',
-                height: "80px",
-                cursor: 'pointer',
-                borderRadius: '0.5rem',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-                display: 'flex',
-                fontSize: '2rem',
-                alignItems: 'center',
-                justifyContent: 'center',
-                // backgroundColor: selectedMonth === s ? '#0051FE' : '',
-                // color: selectedMonth === s ? 'white' : '',
-                // boxShadow: selectedMonth === s ? '0 4px 6px rgba(0, 0, 0, 0.1)' : '',
-                // border: selectedMonth === s ? '1px solid #0051FE' : ''
-              }}
-            >
-              12m
-            </div>
-          </div>
-        </div>
-        <div style={{ display: 'flex', marginTop: "50px", alignItems: 'center', padding: '0.5rem 0', justifyContent: 'space-between' }}>
-          <div style={{ fontSize: '2.5rem', fontWeight: '600' }}>Subscribe for</div>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'center' }}>
-            <div
-              // onClick={() => {
-              //   setSelectedMonth(s);
-              // }}
-              style={{
-                width: '150px',
-                marginLeft: "10px",
-                height: "80px",
-                cursor: 'pointer',
-                borderRadius: '0.5rem',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-                display: 'flex',
-                fontSize: '2rem',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#0051FE',
-                color: 'white',
-                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-                border: '1px solid #0051FE'
-              }}
-            >
-              {model?.value}&nbsp;USDC
-            </div>
-          </div>
-        </div>
-      </div>
-    ),
-    intents: [
-      <Button.Link href={`https://onlytease.vercel.app/profile/${modelId}`}>Subscription successful</Button.Link>,
-      <Button.Link href={`https://testnets.opensea.io/assets/base-sepolia/0x054ba199ef61ef15226e2ceb61138f7d5e2f8408/${tokenId}`}>View your NFT</Button.Link>,
-    ]
-  })
-})
 
 app.frame('/finish', (c) => {
-  const modelId = c.req.queries('modelId')?.[0] || '';
-  const subscriptionId = c.req.queries('subscriptionId')?.[0] || '';
-
-
-  const model = [...modelCardData].find((s) => s.id.toString() === modelId.toLowerCase())
-
+  const modelId = c.frameData?.url.split("?")[0].split("#")[1].split("&")[0].split("=")[1]
+  const subscriptionId = c.frameData?.url.split("?")[0].split("#")[1].split("&")[1].split("=")[1]
+  const model = [...modelCardData].find((s) => s.id?.toString() === modelId?.toLowerCase())
 
   return c.res({
-    action: `/final?modelId=${modelId}&subscriptionId=${subscriptionId}`,
+    action: `/final#modelId=${modelId}&subscriptionId=${subscriptionId}`,
     image: (
       <div
         style={{
@@ -385,7 +236,7 @@ app.frame('/finish', (c) => {
           flexDirection: 'row',
           fontSize: '1.25rem',
         }}>
-          <h1>Subscribe to &nbsp; <strong>{model?.name}</strong></h1>
+          <h1>Subscribe to {model?.name}</h1>
           &nbsp;
           <img src={model?.image_url} alt='ava adams' sizes='20' style={{
             width: 200,
@@ -505,11 +356,167 @@ app.frame('/finish', (c) => {
   })
 })
 
+
+app.frame('/final', async (c) => {
+  const modelId = c.frameData?.url.split("?")[0].split("#")[1].split("&")[0].split("=")[1]
+  const model = [...modelCardData].find((s) => s.id?.toString() === modelId?.toLowerCase())
+  const subscriptionId = c.frameData?.url.split("?")[0].split("#")[1].split("&")[1].split("=")[1]
+  const tokenId = Big(modelId).mul(1e18).plus(subscriptionId).toString()
+  console.log("tokenId", tokenId);
+  // const tokenId = (parseInt(modelId || "14") * 1e18) + parseInt(subscriptionId as string);
+
+  // await updateSubscription({
+  //   email: '',
+  //   modelId: modelId,
+  //   subscriptionId: parseInt(subscriptionId)
+  // })
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(252, 183, 255, 0.8) 100%)',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          padding: "20px",
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div style={{
+          display: 'flex',
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          flexDirection: 'row',
+          fontSize: '1.25rem',
+        }}>
+          <h1>You have subscribed to {model?.name}</h1>
+          &nbsp;
+          <img src={model?.image_url} alt='ava adams' sizes='20' style={{
+            width: 200,
+            height: 200,
+            borderRadius: '50%', // Make the image round
+            overflow: 'hidden', // Add overflow hidden
+          }} />
+        </div>
+        <div style={{ display: 'flex', marginTop: "50px", alignItems: 'center', padding: '0.5rem 0', justifyContent: 'space-between' }}>
+          <div style={{ fontSize: '2.5rem', fontWeight: '600' }}>Autopay 🔁</div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'center' }}>
+            <div
+              // onClick={() => {
+              //   setSelectedMonth(s);
+              // }}
+              style={{
+                width: '100px',
+                height: "80px",
+                cursor: 'pointer',
+                borderRadius: '0.5rem',
+                paddingLeft: '10px',
+                paddingRight: '10px',
+                display: 'flex',
+                fontSize: '2rem',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#0051FE',
+                color: 'white',
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                border: '1px solid #0051FE'
+              }}
+            >
+              3m
+            </div>
+            <div
+              // onClick={() => {
+              //   setSelectedMonth(s);
+              // }}
+              style={{
+                width: '100px',
+                height: "80px",
+                cursor: 'pointer',
+                borderRadius: '0.5rem',
+                paddingLeft: '10px',
+                paddingRight: '10px',
+                display: 'flex',
+                fontSize: '2rem',
+                alignItems: 'center',
+                justifyContent: 'center',
+                // backgroundColor: selectedMonth === s ? '#0051FE' : '',
+                // color: selectedMonth === s ? 'white' : '',
+                // boxShadow: selectedMonth === s ? '0 4px 6px rgba(0, 0, 0, 0.1)' : '',
+                // border: selectedMonth === s ? '1px solid #0051FE' : ''
+              }}
+            >
+              6m
+            </div>
+            <div
+              // onClick={() => {
+              //   setSelectedMonth(s);
+              // }}
+              style={{
+                width: '100px',
+                height: "80px",
+                cursor: 'pointer',
+                borderRadius: '0.5rem',
+                paddingLeft: '10px',
+                paddingRight: '10px',
+                display: 'flex',
+                fontSize: '2rem',
+                alignItems: 'center',
+                justifyContent: 'center',
+                // backgroundColor: selectedMonth === s ? '#0051FE' : '',
+                // color: selectedMonth === s ? 'white' : '',
+                // boxShadow: selectedMonth === s ? '0 4px 6px rgba(0, 0, 0, 0.1)' : '',
+                // border: selectedMonth === s ? '1px solid #0051FE' : ''
+              }}
+            >
+              12m
+            </div>
+          </div>
+        </div>
+        <div style={{ display: 'flex', marginTop: "50px", alignItems: 'center', padding: '0.5rem 0', justifyContent: 'space-between' }}>
+          <div style={{ fontSize: '2.5rem', fontWeight: '600' }}>Subscribe for</div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'center' }}>
+            <div
+              // onClick={() => {
+              //   setSelectedMonth(s);
+              // }}
+              style={{
+                width: '150px',
+                marginLeft: "10px",
+                height: "80px",
+                cursor: 'pointer',
+                borderRadius: '0.5rem',
+                paddingLeft: '10px',
+                paddingRight: '10px',
+                display: 'flex',
+                fontSize: '2rem',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#0051FE',
+                color: 'white',
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                border: '1px solid #0051FE'
+              }}
+            >
+              {model?.value}&nbsp;USDC
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Link href={`https://onlytease.vercel.app/profile/${model?.slug}`}>Subscription successful</Button.Link>,
+      <Button.Link href={`https://testnets.opensea.io/assets/base-sepolia/0x054ba199ef61ef15226e2ceb61138f7d5e2f8408/${tokenId}`}>View your NFT</Button.Link>,
+    ]
+  })
+})
+
 app.transaction('/mint', async (c) => {
   const modelId = c.req.queries('modelId')?.[0] || '';
   const subscriptionId = c.req.queries('subscriptionId')?.[0] || '';
-
-
 
   return c.contract({
     abi: NFT_MARKETPLACE_ABI,
@@ -523,7 +530,6 @@ app.transaction('/mint', async (c) => {
 })
 
 app.transaction('/approve', neynarMiddleware, async (c) => {
-  console.log(c.var.interactor?.profile, "apprive")
   const amount = parseFloat(c.req.queries('amount') ? c.req.queries('amount') as unknown as string : '') * Math.pow(10, 6)
   const amountToApprove = BigInt(amount);
   return c.contract({
